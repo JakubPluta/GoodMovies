47850f8b1e61ecb22d9aaeb9e5d64c3afd1e5f19
Jakub Pluta <plutakuba@gmail.com>
2020-06-07 18:49:51 +0200

    Add views and basic html templates

--- goodmovies/mymovies/urls.py
+++ goodmovies/mymovies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.fetch_movie_from_api),
+    path('', views.index,name='search'),
 ]
--- goodmovies/mymovies/views.py
+++ goodmovies/mymovies/views.py
@@ -1,39 +1,58 @@
-from django.shortcuts import render
 import requests
 import os
+from django.shortcuts import render
 
 TMDb_KEY = os.environ.get('TMDb')
 
 
+def index(request):
+    movies_data = []
+    MAX_RECORDS = 10
 
-# Create your views here.
+    if request.method == "POST":
+        url = f'https://api.themoviedb.org/3/search/movie?'
+        movie_url = f'https://api.themoviedb.org/3/movie/'
 
-# def index(request):
-#     return render(request, 'mymovies/home.html')
-
-
-def fetch_movie_from_api(request=1):
-    url = f'https://api.themoviedb.org/3/movie/550?api_key={TMDb_KEY}'
-    r = requests.get(url).json()
-
-    movie = {
-        'id' : r['id'],
-        "imdb_id": r['imdb_id'],
-        'adult' : r['adult'],
-        'original_language' : r['original_language'],
-        "original_title": r['original_title'],
-        'budget' : r['budget'],
-        "overview" : r["overview"],
-        'popularity' : r['popularity'],
-        'img' : r['backdrop_path'],
-        'genres' :  [genre.get('name') for genre in r['genres']],
-        'status' : r['status'],
-        'tagline' : r['tagline'],
-        "title": r['title'],
-        "vote_average": r['vote_average'],
-        "vote_count": r["vote_count"],
-    }
-    context = {'movie' : movie}
-    return render(request, 'mymovies/home.html',context)
+        query_params = {
+            'query' : request.POST["movie_name"],
+            'page' : "1",
+            'language' : 'en-US',
+            'api_key' : TMDb_KEY
+        }
+        req = requests.get(url, params=query_params)
+        results = req.json()['results']
+        movie_ids = [result.get('id') for result in results][:MAX_RECORDS]
 
 
+        for movie_id in movie_ids:
+            single_movie_url = f"{movie_url}{movie_id}?api_key={TMDb_KEY}"
+            req_movie = requests.get(single_movie_url)
+            single_movie_result = req_movie.json()
+            movie_data = {
+                'id' : single_movie_result.get('id',None),
+                'adult' : single_movie_result.get('adult',None),
+                'budget': single_movie_result.get('budget', None),
+                'genres': single_movie_result.get('genres', None),
+                'title': single_movie_result.get('title', None),
+                'original_title': single_movie_result.get('original_title', None),
+                'original_language': single_movie_result.get('original_language', None),
+                'overview' : single_movie_result.get('overview', None),
+                'popularity': single_movie_result.get('popularity', None),
+                'video': single_movie_result.get('video', None),
+                'vote_count': single_movie_result.get('vote_count', None),
+                'revenue': single_movie_result.get('vote_average', None),
+                'runtime': single_movie_result.get('runtime', None),
+                'vote_average': single_movie_result.get('vote_average', None),
+                'poster_path' : f"https://image.tmdb.org/t/p/original{single_movie_result.get('poster_path',None)}",
+                'backdrop_path':  f"https://image.tmdb.org/t/p/original{single_movie_result.get('backdrop_path', None)}",
+                'release_date': single_movie_result.get('release_date', None),
+            }
+
+            movies_data.append(movie_data)
+
+    context = {
+            'movies_data' : movies_data
+        }
+
+    #return render(request, 'mymovies/search.html', context)
+    return render(request, 'mymovies/home.html', context)
