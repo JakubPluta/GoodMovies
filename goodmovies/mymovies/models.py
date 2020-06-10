0b93721349d0c5769ea183fb57734f8d75fbac45
Jakub Pluta <plutakuba@gmail.com>
2020-06-10 22:48:49 +0200

    Add new template, and some css style

--- goodmovies/mymovies/models.py
+++ goodmovies/mymovies/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+# https://docs.djangoproject.com/en/3.0/ref/models/instances/
+
--- goodmovies/mymovies/urls.py
+++ goodmovies/mymovies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# https://schier.co/blog/html-templating-output-a-grid-in-a-single-loop
+
+
 urlpatterns = [
     path('', views.index,name='search'),
 ]
